991ba7345d0ab0be9d030b02dcd865fe5b3648be
badboy17official <techin187@gmail.com>
2026-04-11 10:59:34 +0530

    fix: remove build artifacts causing CI failure

--- build/CMakeFiles/third_person_shooter.dir/engine/graphics/Renderer.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/third_person_shooter.dir/engine/graphics/Renderer.cpp.o: \
- /home/DeAd_SeC/Project\ Orange/third-person-shooter-engine/engine/graphics/Renderer.cpp \
- /usr/include/stdc-predef.h \
- /home/DeAd_SeC/Project\ Orange/third-person-shooter-engine/engine/graphics/Renderer.h
--- build/CMakeFiles/third_person_shooter.dir/engine/physics/Physics.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/third_person_shooter.dir/engine/physics/Physics.cpp.o: \
- /home/DeAd_SeC/Project\ Orange/third-person-shooter-engine/engine/physics/Physics.cpp \
- /usr/include/stdc-predef.h \
- /home/DeAd_SeC/Project\ Orange/third-person-shooter-engine/engine/physics/Physics.h
